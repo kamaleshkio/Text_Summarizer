7128c58988459dd204b7ce8275b1ac0b8a7047c1
kamalesh <kamaleshbaskaran4310@gmail.com>
2025-04-01 00:00:29 +0530

    model ecaluation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.textSummarizer.pipeline.stage_1_data_ingestion import DataIngestionPipeline
 from src.textSummarizer.pipeline.stage_2_data_validation import DataValidationPipeline
 from src.textSummarizer.pipeline.stage_3_data_tranformation import DataTransformationPipeline
+from src.textSummarizer.pipeline.stage_4_model_trainer import ModelTrainertrainingPipeline
 from src.textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -42,3 +43,15 @@ except Exception as e:
     logger.exception(f"Error occurred during {STAGE_NAME} execution: {e}")
     raise e
 
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_traansformation = ModelTrainertrainingPipeline()
+    data_traansformation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+
+except Exception as e:
+    logger.exception(f"Error occurred during {STAGE_NAME} execution: {e}")
+    raise e
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -5,8 +5,8 @@ from textSummarizer.constants import *
 
 from textSummarizer.entity import DataIngestionConfig
 from textSummarizer.utils.common import create_directories, read_yaml
-from textSummarizer.entity import DataValidationConfig
-from textSummarizer.entity import DataTransformationConfig
+from textSummarizer.entity import (DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig)
+
 
 class ConfigurationManager:
     def __init__(
@@ -47,6 +47,7 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+        
     
 
     def get_data_transformation_config(self) -> DataTransformationConfig:
@@ -61,3 +62,41 @@ class ConfigurationManager:
             )
 
             return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+          config = self.config.model_trainer
+          params = self.params.TrainingArguments
+
+          create_directories([config.root_dir])
+
+          model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_ckpt = config.model_ckpt,
+            num_train_epochs = params.num_train_epochs,
+            warmup_steps = params.warmup_steps,
+            per_device_train_batch_size = params.per_device_train_batch_size,
+            weight_decay = params.weight_decay,
+            logging_steps = params.logging_steps,
+            evaluation_strategy = params.evaluation_strategy,
+            eval_steps = params.eval_steps,
+            save_steps = params.save_steps,
+            gradient_accumulation_steps = params.gradient_accumulation_steps
+        )
+
+          return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+          config = self.config.model_evaluation
+
+          create_directories([config.root_dir])
+
+          model_evaluation_config = ModelEvaluationConfig(
+              root_dir = config.root_dir,
+              data_path = config.data_path,
+              model_path = config.model_path,
+              tokenizer_path = config.tokenizer_path,
+              metrics_file_name = config.metrics_file_name
+          )
+
+          return model_evaluation_config
