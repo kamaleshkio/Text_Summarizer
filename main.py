591653037cdb854ca0140784bddb29a8ab9bad03
kamalesh <kamaleshbaskaran4310@gmail.com>
2025-04-01 00:10:01 +0530

    data ModelEvaluation metric

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.textSummarizer.pipeline.stage_1_data_ingestion import DataIngestionPipe
 from src.textSummarizer.pipeline.stage_2_data_validation import DataValidationPipeline
 from src.textSummarizer.pipeline.stage_3_data_tranformation import DataTransformationPipeline
 from src.textSummarizer.pipeline.stage_4_model_trainer import ModelTrainertrainingPipeline
+from src.textSummarizer.pipeline.stage_5_model_evaluation import ModelEvaluationPipeline  
 from src.textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -49,6 +50,20 @@ STAGE_NAME = "Model Trainer Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_traansformation = ModelTrainertrainingPipeline()
+    data_traansformation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+
+except Exception as e:
+    logger.exception(f"Error occurred during {STAGE_NAME} execution: {e}")
+    raise e
+
+
+
+STAGE_NAME = "Model Evaluation Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_traansformation = ModelEvaluationPipeline()
     data_traansformation.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
 
